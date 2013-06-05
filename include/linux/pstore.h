87563550bda21c269bb4e848ce69bded45168cab
Aruna Balakrishnaiah <aruna@linux.vnet.ibm.com>
2013-06-06 00:22:10 +0530

    powerpc/pseries: Read of-config partition via pstore
    This patch set exploits the pstore subsystem to read details of
    of-config partition in NVRAM to a separate file in /dev/pstore.
    For instance, of-config partition details will be stored in a
    file named [of-nvram-5].
    
    Signed-off-by: Aruna Balakrishnaiah <aruna@linux.vnet.ibm.com>
    Reviewed-by: Jim Keniston <jkenisto@us.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/linux/pstore.h
+++ include/linux/pstore.h
@@ -37,7 +37,8 @@ enum pstore_type_id {
 	PSTORE_TYPE_FTRACE	= 3,
 	/* PPC64 partition types */
 	PSTORE_TYPE_PPC_RTAS	= 4,
-	PSTORE_TYPE_PMSG	= 5, /* Backport: 7 in upstream 3.19.0-rc3 */
+	PSTORE_TYPE_PPC_OF	= 5,
+	PSTORE_TYPE_PMSG	= 6, /* Backport: 7 in upstream 3.19.0-rc3 */
 	PSTORE_TYPE_UNKNOWN	= 255
 };
 
