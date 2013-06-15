b012f3e712e3bc2b27977692004b50b3d3a5b9bc
Peter Hurley <peter@hurleysoftware.com>
2013-06-15 09:36:06 -0400

    tty: Use lockless flip buffer free list
    In preparation for lockless flip buffers, make the flip buffer
    free list lockless.
    
    NB: using llist is not the optimal solution, as the driver and
    buffer work may contend over the llist head unnecessarily. However,
    test measurements indicate this contention is low.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -10,6 +10,7 @@
 #include <linux/mutex.h>
 #include <linux/tty_flags.h>
 #include <uapi/linux/tty.h>
+#include <linux/llist.h>
 
 
 
@@ -29,7 +30,10 @@
 #define __DISABLED_CHAR '\0'
 
 struct tty_buffer {
-	struct tty_buffer *next;
+	union {
+		struct tty_buffer *next;
+		struct llist_node free;
+	};
 	int used;
 	int size;
 	int commit;
@@ -64,7 +68,7 @@ struct tty_bufhead {
 	spinlock_t lock;
 	struct tty_buffer *head;	/* Queue head */
 	struct tty_buffer *tail;	/* Active buffer */
-	struct tty_buffer *free;	/* Free queue head */
+	struct llist_head free;		/* Free queue head */
 	int memory_used;		/* Buffer space used excluding
 								free queue */
 };
