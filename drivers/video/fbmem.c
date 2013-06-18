2d7a49caba683f3122dc0d084c33afc31c792779
Vincent Stehlé <vincent.stehle@freescale.com>
2013-06-18 16:23:05 +0200

    fb: make fp_get_options name argument const
    drm_get_connector_name now returns a const value, which causes the following
    compilation warning:
    
      drivers/gpu/drm/drm_fb_helper.c: In function ‘drm_fb_helper_parse_command_line’:
      drivers/gpu/drm/drm_fb_helper.c:127:3: warning: passing argument 1 of ‘fb_get_options’ discards ‘const’ qualifier from pointer target type [enabled by default]
      In file included from drivers/gpu/drm/drm_fb_helper.c:35:0:
      include/linux/fb.h:627:12: note: expected ‘char *’ but argument is of type ‘const char *’
    
    As fb_get_options uses its name argument as read only, make it const. This
    fixes the aforementioned compilation warning.
    
    Signed-off-by: Vincent Stehlé <vincent.stehle@freescale.com>
    Cc: Jean-Christophe Plagniol-Villard <plagnioj@jcrosoft.com>
    Cc: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Cc: Dave Airlie <airlied@redhat.com>
    Cc: trivial@kernel.org
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -1893,7 +1893,7 @@ static int ofonly __read_mostly;
  *
  * NOTE: Needed to maintain backwards compatibility
  */
-int fb_get_options(char *name, char **option)
+int fb_get_options(const char *name, char **option)
 {
 	char *opt, *options = NULL;
 	int retval = 0;
