2cff883bca8cd67226236077dfc6b7e26e3f2e98
Jingoo Han <jg1.han@samsung.com>
2013-07-30 17:20:47 +0900

    regulator: use dev_get_platdata()
    Use the wrapper function for retrieving the platform data instead of
    accessing dev->platform_data directly.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>

--- drivers/regulator/fan53555.c
+++ drivers/regulator/fan53555.c
@@ -584,7 +584,7 @@ static int fan53555_regulator_probe(struct i2c_client *client,
 	if (client->dev.of_node)
 		pdata = fan53555_get_of_platform_data(client);
 	else
-		pdata = client->dev.platform_data;
+		pdata = dev_get_platdata(&client->dev);
 
 	if (!pdata || !pdata->regulator) {
 		dev_err(&client->dev, "Platform data not found!\n");
