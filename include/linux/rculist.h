0d53e471b17fc4e0ae67abb205031334046383b1
Teodora Baluta <teobaluta@gmail.com>
2013-11-11 17:11:23 +0200

    rcu: Remove "extern" from function declarations in include/linux/*rcu*.h
    Function prototypes don't need to have the "extern" keyword since this
    is the default behavior. Its explicit use is redundant.  This commit
    therefore removes them.
    
    Signed-off-by: Teodora Baluta <teobaluta@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- include/linux/rculist.h
+++ include/linux/rculist.h
@@ -40,8 +40,8 @@ static inline void __list_add_rcu(struct list_head *new,
 	next->prev = new;
 }
 #else
-extern void __list_add_rcu(struct list_head *new,
-		struct list_head *prev, struct list_head *next);
+void __list_add_rcu(struct list_head *new,
+		    struct list_head *prev, struct list_head *next);
 #endif
 
 /**
