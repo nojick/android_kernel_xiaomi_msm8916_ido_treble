eab4f34cde6cd1033fde2c6da6746e10592a9537
Andrew Morton <akpm@linux-foundation.org>
2013-11-14 14:32:09 -0800

    kernel-provide-a-__smp_call_function_single-stub-for-config_smp-fix
    x86_64 allnoconfig:
    
      kernel/up.c:25: error: redefinition of '__smp_call_function_single'
      include/linux/smp.h:154: note: previous definition of '__smp_call_function_single' was here
    
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/smp.h
+++ include/linux/smp.h
@@ -50,6 +50,9 @@ void on_each_cpu_cond(bool (*cond_func)(int cpu, void *info),
 		smp_call_func_t func, void *info, bool wait,
 		gfp_t gfp_flags);
 
+void __smp_call_function_single(int cpuid, struct call_single_data *data,
+				int wait);
+
 #ifdef CONFIG_SMP
 
 #include <linux/preempt.h>
@@ -95,9 +98,6 @@ extern void smp_cpus_done(unsigned int max_cpus);
 int smp_call_function(smp_call_func_t func, void *info, int wait);
 void smp_call_function_many(const struct cpumask *mask,
 			    smp_call_func_t func, void *info, bool wait);
-
-void __smp_call_function_single(int cpuid, struct call_single_data *data,
-				int wait);
 
 int smp_call_function_any(const struct cpumask *mask,
 			  smp_call_func_t func, void *info, int wait);
