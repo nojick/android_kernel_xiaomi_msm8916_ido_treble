e0e95aadacfe3fa8f26e43fff4b7f7d87972a0fb
Luck, Tony <tony.luck@intel.com>
2013-12-18 15:17:10 -0800

    pstore: Don't allow high traffic options on fragile devices
    Some pstore backing devices use on board flash as persistent
    storage. These have limited numbers of write cycles so it
    is a poor idea to use them from high frequency operations.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -443,9 +443,12 @@ int pstore_register(struct pstore_info *psi)
 		pstore_get_records(0);
 
 	kmsg_dump_register(&pstore_dumper);
-	pstore_register_console();
-	pstore_register_ftrace();
-	pstore_register_pmsg();
+
+	if ((psi->flags & PSTORE_FLAGS_FRAGILE) == 0) {
+		pstore_register_console();
+		pstore_register_ftrace();
+		pstore_register_pmsg();
+	}
 
 	if (pstore_update_ms >= 0) {
 		pstore_timer.expires = jiffies +
