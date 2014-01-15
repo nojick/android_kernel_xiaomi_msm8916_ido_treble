fccc695f67a9623cb8a96329fc1668f6befbbc35
Lee, Chun-Yi <joeyli.kernel@gmail.com>
2014-01-15 15:25:48 +0800

    ACPI / init: Run acpi_early_init() before timekeeping_init()
    This is a variant patch from Rafael J. Wysocki's
    ACPI / init: Run acpi_early_init() before efi_enter_virtual_mode()
    
    According to Matt Fleming, if acpi_early_init() was executed before
    efi_enter_virtual_mode(), the EFI initialization could benefit from
    it, so Rafael's patch makes that happen.
    
    And, we want accessing ACPI TAD device to set system clock, so move
    acpi_early_init() before timekeeping_init(). This final position is
    also before efi_enter_virtual_mode().
    
    Tested-by: Toshi Kani <toshi.kani@hp.com>
    Signed-off-by: Lee, Chun-Yi <jlee@suse.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- init/main.c
+++ init/main.c
@@ -564,6 +564,7 @@ asmlinkage __visible void __init start_kernel(void)
 	init_timers();
 	hrtimers_init();
 	softirq_init();
+	acpi_early_init();
 	timekeeping_init();
 	time_init();
 	perf_event_init();
@@ -644,7 +645,6 @@ asmlinkage __visible void __init start_kernel(void)
 
 	check_bugs();
 
-	acpi_early_init(); /* before LAPIC and SMP init */
 	sfi_init_late();
 
 	if (efi_enabled(EFI_RUNTIME_SERVICES)) {
