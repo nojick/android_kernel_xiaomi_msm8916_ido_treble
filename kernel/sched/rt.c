a1965c6edd8e9c1f061fb4ad4595a7c9f31e8d70
Li Zefan <lizefan@huawei.com>
2014-02-08 14:17:45 +0800

    sched/rt: Make init_sched_rt_calss() __init
    It's a bootstrap function.
    
    Signed-off-by: Li Zefan <lizefan@huawei.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/52F5CC09.1080502@huawei.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2024,7 +2024,7 @@ static void switched_from_rt(struct rq *rq, struct task_struct *p)
 		resched_task(rq->curr);
 }
 
-void init_sched_rt_class(void)
+void __init init_sched_rt_class(void)
 {
 	unsigned int i;
 
