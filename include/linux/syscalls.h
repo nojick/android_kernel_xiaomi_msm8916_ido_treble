551d7417c580f397fbfcf21ee30d1d88e6b698e2
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-04-09 17:06:08 -0400

    tracing: Fix anonymous unions in struct ftrace_event_call
    gcc <= 4.5.x has significant limitations with respect to initialization
    of anonymous unions within structures. They need to be surrounded by
    brackets, _and_ they need to be initialized in the same order in which
    they appear in the structure declaration.
    
    Link: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=10676
    Link: http://lkml.kernel.org/r/1397077568-3156-1-git-send-email-mathieu.desnoyers@efficios.com
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -117,8 +117,10 @@ extern struct trace_event_functions exit_syscall_print_funcs;
 	static struct syscall_metadata __syscall_meta_##sname;		\
 	static struct ftrace_event_call __used				\
 	  event_enter_##sname = {					\
-		.name                   = "sys_enter"#sname,		\
 		.class			= &event_class_syscall_enter,	\
+		{							\
+			.name                   = "sys_enter"#sname,	\
+		},							\
 		.event.funcs            = &enter_syscall_print_funcs,	\
 		.data			= (void *)&__syscall_meta_##sname,\
 		.flags                  = TRACE_EVENT_FL_CAP_ANY,	\
@@ -131,8 +133,10 @@ extern struct trace_event_functions exit_syscall_print_funcs;
 	static struct syscall_metadata __syscall_meta_##sname;		\
 	static struct ftrace_event_call __used				\
 	  event_exit_##sname = {					\
-		.name                   = "sys_exit"#sname,		\
 		.class			= &event_class_syscall_exit,	\
+		{							\
+			.name                   = "sys_exit"#sname,	\
+		},							\
 		.event.funcs		= &exit_syscall_print_funcs,	\
 		.data			= (void *)&__syscall_meta_##sname,\
 		.flags                  = TRACE_EVENT_FL_CAP_ANY,	\
