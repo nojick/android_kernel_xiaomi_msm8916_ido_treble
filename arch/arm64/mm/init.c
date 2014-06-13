39a55e9892687bc52142d960207beb16c2513e29
Catalin Marinas <catalin.marinas@arm.com>
2014-06-13 13:41:20 +0100

    arm64: Limit the CMA buffer to 32-bit if ZONE_DMA
    When the CMA buffer is allocated, it is too early to know whether
    devices will require ZONE_DMA memory. This patch limits the CMA buffer
    to (DMA_BIT_MASK(32) + 1) if CONFIG_ZONE_DMA is enabled.
    
    In addition, it computes the dma_to_phys(DMA_BIT_MASK(32)) before the
    increment (no current functional change).
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Git-commit: 2d5a5612bceda8edd25b29f363c4e2c6cda28bab
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -71,7 +71,7 @@ static void __init zone_sizes_init(unsigned long min, unsigned long max)
 	/* 4GB maximum for 32-bit only capable devices */
 	if (IS_ENABLED(CONFIG_ZONE_DMA)) {
 		unsigned long max_dma_phys =
-			(unsigned long)dma_to_phys(NULL, DMA_BIT_MASK(32) + 1);
+			(unsigned long)(dma_to_phys(NULL, DMA_BIT_MASK(32)) + 1);
 		max_dma = max(min, min(max, max_dma_phys >> PAGE_SHIFT));
 		zone_size[ZONE_DMA] = max_dma - min;
 	}
