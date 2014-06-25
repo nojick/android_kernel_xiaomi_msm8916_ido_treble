94271f59ddf0f76a804c2b459289d86622bb7d05
Steve Capper <steve.capper@linaro.org>
2014-06-25 08:41:45 +0100

    arm64: mm: Fix horrendous config typo
    The define ARM64_64K_PAGES is tested for rather than
    CONFIG_ARM64_64K_PAGES. Correct that typo here.
    
    Signed-off-by: Steve Capper <steve.capper@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -337,7 +337,7 @@ extern pgprot_t phys_mem_access_prot(struct file *file, unsigned long pfn,
 #define pmd_sect(pmd)		((pmd_val(pmd) & PMD_TYPE_MASK) == \
 				 PMD_TYPE_SECT)
 
-#ifdef ARM64_64K_PAGES
+#ifdef CONFIG_ARM64_64K_PAGES
 #define pud_sect(pud)		(0)
 #define pud_table(pud)		(1)
 #else
