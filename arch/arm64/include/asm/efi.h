02244120f978c16e591987fd7dfeb9da130ef6a6
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2014-07-04 19:41:53 +0200

    efi/arm64: Preserve FP/SIMD registers on UEFI runtime services calls
    According to the UEFI spec section 2.3.6.4, the use of FP/SIMD
    instructions is allowed, and should adhere to the AAPCS64 calling
    convention, which states that 'only the bottom 64 bits of each value
    stored in registers v8-v15 need to be preserved' (section 5.1.2).
    
    This applies equally to UEFI Runtime Services called by the kernel, so
    make sure the FP/SIMD register file is preserved in this case. We do this
    by enabling the wrappers for UEFI Runtime Services (CONFIG_EFI_RUNTIME_WRAPPERS)
    and inserting calls to kernel_neon_begin()and kernel_neon_end() into
    these wrappers.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- arch/arm64/include/asm/efi.h
+++ arch/arm64/include/asm/efi.h
@@ -2,11 +2,32 @@
 #define _ASM_EFI_H
 
 #include <asm/io.h>
+#include <asm/neon.h>
 
 #ifdef CONFIG_EFI
 extern void efi_init(void);
 #else
 #define efi_init()
 #endif
+
+#define efi_call_virt(f, ...)						\
+({									\
+	efi_##f##_t *__f = efi.systab->runtime->f;			\
+	efi_status_t __s;						\
+									\
+	kernel_neon_begin();						\
+	__s = __f(__VA_ARGS__);						\
+	kernel_neon_end();						\
+	__s;								\
+})
+
+#define __efi_call_virt(f, ...)						\
+({									\
+	efi_##f##_t *__f = efi.systab->runtime->f;			\
+									\
+	kernel_neon_begin();						\
+	__f(__VA_ARGS__);						\
+	kernel_neon_end();						\
+})
 
 #endif /* _ASM_EFI_H */
