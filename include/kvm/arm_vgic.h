e173870bdab26badacc5c257981513c192d83334
Marc Zyngier <marc.zyngier@arm.com>
2014-07-08 12:09:03 +0100

    arm/arm64: KVM: vgic: kill VGIC_MAX_CPUS
    We now have the information about the number of CPU interfaces in
    the distributor itself. Let's get rid of VGIC_MAX_CPUS, and just
    rely on KVM_MAX_VCPUS where we don't have the choice. Yet.
    
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    (cherry picked from commit fc675e355e705a046df7b635d3f3330c0ad94569)
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- include/kvm/arm_vgic.h
+++ include/kvm/arm_vgic.h
@@ -29,12 +29,11 @@
 #define VGIC_NR_SGIS		16
 #define VGIC_NR_PPIS		16
 #define VGIC_NR_PRIVATE_IRQS	(VGIC_NR_SGIS + VGIC_NR_PPIS)
-#define VGIC_MAX_CPUS		KVM_MAX_VCPUS
 
 #define VGIC_V2_MAX_LRS		(1 << 6)
 
 /* Sanity checks... */
-#if (VGIC_MAX_CPUS > 8)
+#if (KVM_MAX_VCPUS > 8)
 #error	Invalid number of CPU interfaces
 #endif
 
