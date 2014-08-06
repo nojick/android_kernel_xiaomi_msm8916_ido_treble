0351dc74eafa0804d7e96e9ee6a99d2c83132a50
Laura Abbott <lauraa@codeaurora.org>
2014-08-05 18:47:54 -0700

    drivers: Add dma removed ops
    The current DMA coherent pool assumes that there is a kernel
    mapping at all times for hte entire pool. This may not be
    what we want for the entire times. Add the dma_removed ops to
    support this use case.
    
    Change-Id: Ie4f1e9bdf57b79699fa8fa7e7a6087e6d88ebbfa
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/base/dma-removed.c
+++ drivers/base/dma-removed.c
@@ -23,8 +23,54 @@
 #include <linux/io.h>
 #include <linux/vmalloc.h>
 #include <linux/sizes.h>
+#include <linux/spinlock.h>
+
+struct removed_region {
+	dma_addr_t	base;
+	int		nr_pages;
+	unsigned long	*bitmap;
+	struct mutex	lock;
+};
 
 #define NO_KERNEL_MAPPING_DUMMY	0x2222
+
+static int dma_init_removed_memory(phys_addr_t phys_addr, size_t size,
+				struct removed_region **mem)
+{
+	struct removed_region *dma_mem = NULL;
+	int pages = size >> PAGE_SHIFT;
+	int bitmap_size = BITS_TO_LONGS(pages) * sizeof(long);
+
+	dma_mem = kzalloc(sizeof(struct removed_region), GFP_KERNEL);
+	if (!dma_mem)
+		goto out;
+	dma_mem->bitmap = kzalloc(bitmap_size, GFP_KERNEL);
+	if (!dma_mem->bitmap)
+		goto free1_out;
+
+	dma_mem->base = phys_addr;
+	dma_mem->nr_pages = pages;
+	mutex_init(&dma_mem->lock);
+
+	*mem = dma_mem;
+
+	return 0;
+
+free1_out:
+	kfree(dma_mem);
+out:
+	return -ENOMEM;
+}
+
+static int dma_assign_removed_region(struct device *dev,
+					struct removed_region *mem)
+{
+	if (dev->removed_mem)
+		return -EBUSY;
+
+	dev->removed_mem = mem;
+	return 0;
+}
 
 void *removed_alloc(struct device *dev, size_t size, dma_addr_t *handle,
 		    gfp_t gfp, struct dma_attrs *attrs)
@@ -32,26 +78,42 @@ void *removed_alloc(struct device *dev, size_t size, dma_addr_t *handle,
 	bool no_kernel_mapping = dma_get_attr(DMA_ATTR_NO_KERNEL_MAPPING,
 					attrs);
 	bool skip_zeroing = dma_get_attr(DMA_ATTR_SKIP_ZEROING, attrs);
-	struct page *pfn;
+	int pageno;
 	unsigned long order = get_order(size);
 	void *addr = NULL;
+	struct removed_region *dma_mem = dev->removed_mem;
+	int nbits = size >> PAGE_SHIFT;
+	unsigned int align;
 
 	size = PAGE_ALIGN(size);
 
 	if (!(gfp & __GFP_WAIT))
 		return NULL;
 
-	pfn = dma_alloc_from_contiguous(dev, size >> PAGE_SHIFT, order);
+	if (order > get_order(SZ_1M))
+		order = get_order(SZ_1M);
 
-	if (pfn) {
+	align = (1 << order) - 1;
+
+
+	mutex_lock(&dma_mem->lock);
+	pageno = bitmap_find_next_zero_area(dma_mem->bitmap, dma_mem->nr_pages,
+						0, nbits, align);
+
+	if (pageno < dma_mem->nr_pages) {
+		phys_addr_t base = dma_mem->base + pageno * PAGE_SIZE;
+		*handle = base;
+
+		bitmap_set(dma_mem->bitmap, pageno, nbits);
+
 		if (no_kernel_mapping && skip_zeroing) {
-			*handle = __pfn_to_phys(pfn);
-			return (void *)NO_KERNEL_MAPPING_DUMMY;
+			addr = (void *)NO_KERNEL_MAPPING_DUMMY;
+			goto out;
 		}
 
-		addr = ioremap(__pfn_to_phys(pfn), size);
+		addr = ioremap(base, size);
 		if (WARN_ON(!addr)) {
-			dma_release_from_contiguous(dev, pfn, order);
+			bitmap_clear(dma_mem->bitmap, pageno, nbits);
 		} else {
 			if (!skip_zeroing)
 				memset(addr, 0, size);
@@ -59,10 +121,12 @@ void *removed_alloc(struct device *dev, size_t size, dma_addr_t *handle,
 				iounmap(addr);
 				addr = (void *)NO_KERNEL_MAPPING_DUMMY;
 			}
-			*handle = __pfn_to_phys(pfn);
+			*handle = base;
 		}
 	}
 
+out:
+	mutex_unlock(&dma_mem->lock);
 	return addr;
 }
 
@@ -79,9 +143,14 @@ void removed_free(struct device *dev, size_t size, void *cpu_addr,
 {
 	bool no_kernel_mapping = dma_get_attr(DMA_ATTR_NO_KERNEL_MAPPING,
 					attrs);
+	struct removed_region *dma_mem = dev->removed_mem;
 
 	if (!no_kernel_mapping)
 		iounmap(cpu_addr);
+	mutex_lock(&dma_mem->lock);
+	bitmap_clear(dma_mem->bitmap, (handle - dma_mem->base) >> PAGE_SHIFT,
+				size >> PAGE_SHIFT);
+	mutex_unlock(&dma_mem->lock);
 }
 
 static dma_addr_t removed_map_page(struct device *dev, struct page *page,
@@ -170,4 +239,42 @@ struct dma_map_ops removed_dma_ops = {
 };
 EXPORT_SYMBOL(removed_dma_ops);
 
+#ifdef CONFIG_OF_RESERVED_MEM
+#include <linux/of.h>
+#include <linux/of_fdt.h>
+#include <linux/of_reserved_mem.h>
 
+static int rmem_dma_device_init(struct reserved_mem *rmem, struct device *dev)
+{
+        struct removed_region *mem = rmem->priv;
+        if (!mem && dma_init_removed_memory(rmem->base, rmem->size, &mem)) {
+                pr_info("Reserved memory: failed to init DMA memory pool at %pa, size %ld MiB\n",
+                        &rmem->base, (unsigned long)rmem->size / SZ_1M);
+                return -EINVAL;
+        }
+	set_dma_ops(dev, &removed_dma_ops);
+        rmem->priv = mem;
+        dma_assign_removed_region(dev, mem);
+	return 0;
+}
+
+static void rmem_dma_device_release(struct reserved_mem *rmem,
+                                    struct device *dev)
+{
+        dev->dma_mem = NULL;
+}
+
+static const struct reserved_mem_ops removed_mem_ops = {
+        .device_init    = rmem_dma_device_init,
+        .device_release = rmem_dma_device_release,
+};
+
+static int __init removed_dma_setup(struct reserved_mem *rmem)
+{
+        rmem->ops = &removed_mem_ops;
+        pr_info("Removed memory: created DMA memory pool at %pa, size %ld MiB\n",
+                &rmem->base, (unsigned long)rmem->size / SZ_1M);
+        return 0;
+}
+RESERVEDMEM_OF_DECLARE(dma, "removed-dma-pool", removed_dma_setup);
+#endif
