4fe4a5182001fc0f8a0e78d87bebd3a2f19c0fd3
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-08-19 12:31:54 +0530

    sched: window_stats: Add "disable" mode support
    "disabled" mode (sched_disble_window_stats = 1) disables all
    window-stats related activity. This is useful when changing key
    configuration variables associated with window-stats feature (like
    policy or window size).
    
    Change-Id: I9e55c9eb7f7e3b1b646079c3aa338db6259a9cfe
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -854,6 +854,7 @@ extern void init_new_task_load(struct task_struct *p);
 
 extern unsigned int sched_ravg_window;
 extern unsigned int sched_use_pelt;
+extern unsigned int sched_disable_window_stats;
 extern unsigned int max_possible_freq;
 extern unsigned int min_max_freq;
 extern unsigned int pct_task_load(struct task_struct *p);
@@ -891,7 +892,7 @@ inc_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
 	if (sched_use_pelt)
 		rq->cumulative_runnable_avg +=
 				p->se.avg.runnable_avg_sum_scaled;
-	else
+	else if (!sched_disable_window_stats)
 		rq->cumulative_runnable_avg += p->ravg.demand;
 }
 
@@ -901,7 +902,7 @@ dec_cumulative_runnable_avg(struct rq *rq, struct task_struct *p)
 	if (sched_use_pelt)
 		rq->cumulative_runnable_avg -=
 				p->se.avg.runnable_avg_sum_scaled;
-	else
+	else if (!sched_disable_window_stats)
 		rq->cumulative_runnable_avg -= p->ravg.demand;
 	BUG_ON((s64)rq->cumulative_runnable_avg < 0);
 }
