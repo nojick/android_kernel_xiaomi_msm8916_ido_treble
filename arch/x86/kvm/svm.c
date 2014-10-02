3c5e5de587374d41a22f858ecb824e88154c7ec3
Christoffer Dall <christoffer.dall@linaro.org>
2014-10-02 22:29:59 +0200

    KVM: remove garbage arg to *hardware_{en,dis}able
    In the beggining was on_each_cpu(), which required an unused argument to
    kvm_arch_ops.hardware_{en,dis}able, but this was soon forgotten.
    
    Remove unnecessary arguments that stem from this.
    
    Signed-off-by: Radim KrÄmÃ¡Å™ <rkrcmar@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 13a34e067eab24fec882e1834fbf2cc31911d474)
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -608,7 +608,7 @@ static int has_svm(void)
 	return 1;
 }
 
-static void svm_hardware_disable(void *garbage)
+static void svm_hardware_disable(void)
 {
 	/* Make sure we clean up behind us */
 	if (static_cpu_has(X86_FEATURE_TSCRATEMSR))
@@ -619,7 +619,7 @@ static void svm_hardware_disable(void *garbage)
 	amd_pmu_disable_virt();
 }
 
-static int svm_hardware_enable(void *garbage)
+static int svm_hardware_enable(void)
 {
 
 	struct svm_cpu_data *sd;
