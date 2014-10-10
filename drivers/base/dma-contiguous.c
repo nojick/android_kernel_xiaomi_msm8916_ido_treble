cf1b4614033e3dd22363bd208db009f271b13d15
Neeti Desai <neetid@codeaurora.org>
2014-10-09 17:31:04 -0700

    cma: Add support for cma_get_size
    HLOS assumes the size of the CMA based heaps is equal to the
    size specified with the "linux-contiguous-region" property.
    This is not always true, and CMA might reserve more memory
    to take care of alignment. This causes the secure world to
    lockdown more memory than it is supposed to, thus denying
    access to clients. The cma_get_size api returns the correct
    size which is reserved for these heaps, thus preventing
    client access issues.
    
    CRs-Fixed: 737584
    Change-Id: Idf5f3587c0a2b1a300b2102079b6796323254cc8
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -512,6 +512,13 @@ phys_addr_t cma_get_base(struct device *dev)
 	return cma->base_pfn << PAGE_SHIFT;
 }
 
+unsigned long cma_get_size(struct device *dev)
+{
+	struct cma *cma = dev_get_cma_area(dev);
+
+	return cma->count << PAGE_SHIFT;
+}
+
 /**
  * dma_alloc_from_contiguous() - allocate pages from contiguous area
  * @dev:   Pointer to device for which the allocation is performed.
