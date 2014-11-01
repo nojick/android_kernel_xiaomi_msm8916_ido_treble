925242f415b21947f13cdf4b15cdc6e80efa5077
Rohit Vaswani <rvaswani@codeaurora.org>
2014-10-31 20:53:08 -0700

    arm64: Add support for KERNEL_TEXT_RDONLY
    When using FORCE_PAGES to allocate the kernel memory into pages,
    provide an option to mark the the kernel text section as read only.
    Since the kernel text pages are always mapped in the kernel, anyone
    can write to the page if they have the address.
    Enable this option to mark the kernel text pages as read only to
    trigger a fault if any code attempts to write to a page that is
    part of the kernel text section.
    
    Change-Id: I2a9e105a3340686b4314bb10cc2a6c7bfa19ce8e
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -35,6 +35,7 @@
 #include <asm/tlb.h>
 #include <asm/memblock.h>
 #include <asm/mmu_context.h>
+#include <asm/cacheflush.h>
 
 #include "mm.h"
 
@@ -552,6 +553,8 @@ void __init paging_init(void)
 	 * point to zero page to avoid speculatively fetching new entries.
 	 */
 	cpu_set_reserved_ttbr0();
+	flush_tlb_all();
+	set_kernel_text_ro();
 	flush_tlb_all();
 }
 
