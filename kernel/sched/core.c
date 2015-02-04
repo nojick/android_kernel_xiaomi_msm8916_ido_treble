1f416716db740e18d00dc372ae034144fbc191a9
Joonwoo Park <joonwoop@codeaurora.org>
2015-02-03 18:10:03 -0800

    sched: fix incorrect wait time and wait count statistics
    Scheduler at present resets task's wait start timestamp when task migrates
    to another rq.  This misleads scheduler itself into reporting less wait
    time than actual by omitting time spent for waiting prior to migration and
    also more wait count than actual by counting migration as wait end event
    which can be seen by trace or /proc/<pid>/sched with CONFIG_SCHEDSTATS=y.
    
    Carry forward migrating task's wait time prior to migration and don't count
    migration as a wait-end event to fix such statistics error.
    
    Change-Id: I0f6badf8072fc37826e4476ac2d1195e82b65bf1
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    [rameezmustafa@codeaurora.org: Port to msm-3.18]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6517,7 +6517,7 @@ static struct rq *move_queued_task(struct task_struct *p, int new_cpu)
 
 	lockdep_assert_held(&rq->lock);
 
-	dequeue_task(rq, p, 0);
+	dequeue_task(rq, p, DEQUEUE_MIGRATING);
 	p->on_rq = TASK_ON_RQ_MIGRATING;
 	set_task_cpu(p, new_cpu);
 	raw_spin_unlock(&rq->lock);
@@ -6527,7 +6527,7 @@ static struct rq *move_queued_task(struct task_struct *p, int new_cpu)
 	raw_spin_lock(&rq->lock);
 	BUG_ON(task_cpu(p) != new_cpu);
 	p->on_rq = TASK_ON_RQ_QUEUED;
-	enqueue_task(rq, p, 0);
+	enqueue_task(rq, p, ENQUEUE_MIGRATING);
 	check_preempt_curr(rq, p, 0);
 
 	return rq;
