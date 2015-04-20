8ef3d369341cf93192ad96faeac9f455ea4a0e59
Archana Sathyakumar <asathyak@codeaurora.org>
2015-04-20 14:01:17 -0600

    soc: qcom: spm: Split SPM/AVS functionality
    SPM DT node supplies all the register information and expects
    initialization value for all of them. With PSCI, most of the SPM
    programming will be done in EL1 secure and only vctl programming will be
    done from HLOS. Change the spm driver to allow write to only those
    registers which has initialization values provided in DT.
    
    Change-Id: I073c1c0487bfa98400e234e32f1cc27f20f3b79e
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/soc/qcom/spm_devices.c
+++ drivers/soc/qcom/spm_devices.c
@@ -250,6 +250,9 @@ static int msm_spm_dev_set_low_power_mode(struct msm_spm_device *dev,
 	if (!dev->initialized)
 		return -ENXIO;
 
+	if (!dev->num_modes)
+		return 0;
+
 	if (mode == MSM_SPM_MODE_DISABLED && set_spm_enable) {
 		ret = msm_spm_drv_set_spm_enable(&dev->reg_data, false);
 	} else {
@@ -290,7 +293,6 @@ static int msm_spm_dev_init(struct msm_spm_device *dev,
 	if (!dev->modes)
 		goto spm_failed_malloc;
 
-	dev->reg_data.ver_reg = data->ver_reg;
 	ret = msm_spm_drv_init(&dev->reg_data, data);
 
 	if (ret)
@@ -310,7 +312,9 @@ static int msm_spm_dev_init(struct msm_spm_device *dev,
 
 		dev->modes[i].mode = data->modes[i].mode;
 	}
-	msm_spm_drv_reinit(&dev->reg_data);
+
+	msm_spm_drv_reinit(&dev->reg_data, dev->num_modes ? true : false);
+
 	dev->initialized = true;
 
 	return 0;
@@ -376,7 +380,8 @@ void msm_spm_reinit(void)
 {
 	unsigned int cpu;
 	for_each_possible_cpu(cpu)
-		msm_spm_drv_reinit(&per_cpu(msm_cpu_spm_device.reg_data, cpu));
+		msm_spm_drv_reinit(
+			&per_cpu(msm_cpu_spm_device.reg_data, cpu), true);
 }
 EXPORT_SYMBOL(msm_spm_reinit);
 
@@ -849,12 +854,14 @@ static int msm_spm_dev_probe(struct platform_device *pdev)
 	 * SPM_LEGACY_MODE bit.
 	 */
 	msm_spm_config_q2s(dev, MSM_SPM_MODE_POWER_COLLAPSE);
+	msm_spm_drv_reg_init(&dev->reg_data, &spm_data);
 
 	for (i = 0; i < ARRAY_SIZE(spm_of_data); i++) {
 		ret = of_property_read_u32(node, spm_of_data[i].key, &val);
 		if (ret)
 			continue;
-		spm_data.reg_init_values[spm_of_data[i].id] = val;
+		msm_spm_drv_upd_reg_shadow(&dev->reg_data, spm_of_data[i].id,
+				val);
 	}
 
 	for_each_child_of_node(node, n) {
@@ -869,7 +876,6 @@ static int msm_spm_dev_probe(struct platform_device *pdev)
 		if (ret)
 			continue;
 
-		pr_err("label name %s\n", name);
 		for (i = 0; i < ARRAY_SIZE(mode_of_data); i++)
 			if (!strcmp(name, mode_of_data[i].key))
 					break;
@@ -917,12 +923,15 @@ static int msm_spm_dev_probe(struct platform_device *pdev)
 
 	ret = msm_spm_dev_init(dev, &spm_data);
 	if (ret)
-		goto fail;
+		pr_err("SPM modes programming is not available from HLOS\n");
 
 	platform_set_drvdata(pdev, dev);
 
 	for_each_cpu(cpu, &dev->mask)
 		per_cpu(cpu_vctl_device, cpu) = dev;
+
+	if (!spm_data.num_modes)
+		return 0;
 
 	cpu = get_cpu_id(pdev->dev.of_node);
 
@@ -935,7 +944,6 @@ static int msm_spm_dev_probe(struct platform_device *pdev)
 		msm_spm_config_low_power_mode(dev, MSM_SPM_MODE_CLOCK_GATING,
 				false);
 	put_online_cpus();
-
 	return ret;
 
 fail:
