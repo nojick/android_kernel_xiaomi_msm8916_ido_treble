9e14f80f8359fad423ddee661cabb216caad373d
Vinay Kalia <vkalia@codeaurora.org>
2015-07-30 15:46:23 -0700

    msm: vidc: Increase output buffer count for DCVS
    To get more residency at lower voltage corner, buffer count needs
    to be increased for resolutions for which DCVS is enabled. This
    change lowers the power consumption during video playback of higher
    resolutions but at the cost of increase in memory.
    
    Change-Id: I2d0adc5e0483bba5c4f4dd6d0070ef8a518a3597
    Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>

--- drivers/media/platform/msm/vidc/msm_vidc_dcvs.c
+++ drivers/media/platform/msm/vidc/msm_vidc_dcvs.c
@@ -559,7 +559,7 @@ int msm_dcvs_get_extra_buff_count(struct msm_vidc_inst *inst,
 
 	if (!inst) {
 		dprintk(VIDC_ERR, "%s Invalid args\n", __func__);
-		return -EINVAL;
+		return 0;
 	}
 
 	if (inst->session_type == MSM_VIDC_ENCODER) {
@@ -570,6 +570,9 @@ int msm_dcvs_get_extra_buff_count(struct msm_vidc_inst *inst,
 					!inst->dcvs.is_output_buff_added)
 				extra_buffer = DCVS_ENC_EXTRA_OUTPUT_BUFFERS;
 		}
+	} else if (inst->session_type == MSM_VIDC_DECODER) {
+		if (msm_dcvs_check_supported(inst))
+			extra_buffer = DCVS_DEC_EXTRA_OUTPUT_BUFFERS;
 	}
 	return extra_buffer;
 }
