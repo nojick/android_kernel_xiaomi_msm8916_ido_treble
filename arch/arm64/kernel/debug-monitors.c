ab134d578c543ba9858cf7c8b5c3bcb11a6ab29a
Suzuki K. Poulose <suzuki.poulose@arm.com>
2015-10-19 14:24:54 +0100

    BACKPORT: arm64/debug: Make use of the system wide safe value
    Use the system wide value of ID_AA64DFR0 to make safer decisions
    
    Signed-off-by: Suzuki K. Poulose <suzuki.poulose@arm.com>
    Tested-by: Dave Martin <Dave.Martin@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 31432001
    Change-Id: Id0d0c664a3de9399c92bf5200f42840fbd402fba
    (cherry picked from commit 3085bb01b40676d946a13064483ab2819ae3b010)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/kernel/debug-monitors.c
+++ arch/arm64/kernel/debug-monitors.c
@@ -26,8 +26,9 @@
 #include <linux/stat.h>
 #include <linux/uaccess.h>
 
-#include <asm/debug-monitors.h>
+#include <asm/cpufeature.h>
 #include <asm/cputype.h>
+#include <asm/debug-monitors.h>
 #include <asm/system_misc.h>
 
 /* Low-level stepping controls. */
@@ -42,7 +43,8 @@
 /* Determine debug architecture. */
 u8 debug_monitors_arch(void)
 {
-	return read_cpuid(ID_AA64DFR0_EL1) & 0xf;
+	return cpuid_feature_extract_field(read_system_reg(SYS_ID_AA64DFR0_EL1),
+						ID_AA64DFR0_DEBUGVER_SHIFT);
 }
 
 /*
