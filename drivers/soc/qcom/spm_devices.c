d7559a13662869965f40caf48efe0d698434e5e3
Archana Sathyakumar <asathyak@codeaurora.org>
2015-10-22 14:05:54 -0600

    drivers: msm-pm: Add SPC flag for standalone power collapse
    Use mode flag to distinguish between standalone power collapse
    and power collapse sequence.
    
    Change-Id: Idfa3586d0c06e8c6d9e922747438dfb109a8213a
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>
    Signed-off-by: Archana Sathyakumar <asathyak@codeaurora.org>

--- drivers/soc/qcom/spm_devices.c
+++ drivers/soc/qcom/spm_devices.c
@@ -174,6 +174,7 @@ static void msm_spm_config_q2s(struct msm_spm_device *dev, unsigned int mode)
 		spm_legacy_mode = 0;
 		break;
 	case MSM_SPM_MODE_GDHS:
+	case MSM_SPM_MODE_STANDALONE_POWER_COLLAPSE:
 	case MSM_SPM_MODE_POWER_COLLAPSE:
 		qchannel_ignore = dev->qchannel_ignore;
 		spm_legacy_mode = 1;
@@ -197,6 +198,7 @@ static void msm_spm_config_hw_flush(struct msm_spm_device *dev,
 
 	switch (mode) {
 	case MSM_SPM_MODE_FASTPC:
+	case MSM_SPM_MODE_STANDALONE_POWER_COLLAPSE:
 	case MSM_SPM_MODE_POWER_COLLAPSE:
 		val = BIT(0);
 		break;
@@ -218,6 +220,7 @@ static void msm_spm_config_slpreq(struct msm_spm_device *dev,
 	switch (mode) {
 	case MSM_SPM_MODE_FASTPC:
 	case MSM_SPM_MODE_GDHS:
+	case MSM_SPM_MODE_STANDALONE_POWER_COLLAPSE:
 	case MSM_SPM_MODE_POWER_COLLAPSE:
 		val = BIT(4);
 		break;
@@ -696,7 +699,8 @@ static int msm_spm_dev_probe(struct platform_device *pdev)
 		{"qcom,saw2-spm-cmd-wfi", MSM_SPM_MODE_CLOCK_GATING},
 		{"qcom,saw2-spm-cmd-ret", MSM_SPM_MODE_RETENTION},
 		{"qcom,saw2-spm-cmd-gdhs", MSM_SPM_MODE_GDHS},
-		{"qcom,saw2-spm-cmd-spc", MSM_SPM_MODE_POWER_COLLAPSE},
+		{"qcom,saw2-spm-cmd-spc",
+				MSM_SPM_MODE_STANDALONE_POWER_COLLAPSE},
 		{"qcom,saw2-spm-cmd-pc", MSM_SPM_MODE_POWER_COLLAPSE},
 		{"qcom,saw2-spm-cmd-fpc", MSM_SPM_MODE_FASTPC},
 	};
