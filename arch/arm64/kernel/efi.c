dfdac4a250d9b686a6c519f0321aa93d1a013a14
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-11-17 09:53:31 +0100

    UPSTREAM: arm64: use non-global mappings for UEFI runtime regions
    As pointed out by Russell King in response to the proposed ARM version
    of this code, the sequence to switch between the UEFI runtime mapping
    and current's actual userland mapping (and vice versa) is potentially
    unsafe, since it leaves a time window between the switch to the new
    page tables and the TLB flush where speculative accesses may hit on
    stale global TLB entries.
    
    So instead, use non-global mappings, and perform the switch via the
    ordinary ASID-aware context switch routines.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 31432001
    Change-Id: Ifcde1db2664a333a262dce9946d7d11c100db66f
    (cherry picked from commit 65da0a8e34a857f2ba9ccb91dc8f8f964cf938b7)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/kernel/efi.c
+++ arch/arm64/kernel/efi.c
@@ -349,6 +349,8 @@ static bool __init efi_virtmap_init(void)
 {
 	efi_memory_desc_t *md;
 
+	init_new_context(NULL, &efi_mm);
+
 	for_each_efi_memory_desc(&memmap, md) {
 		u64 paddr, npages, size;
 		pgprot_t prot;
@@ -378,7 +380,8 @@ static bool __init efi_virtmap_init(void)
 		else
 			prot = PAGE_KERNEL;
 
-		create_pgd_mapping(&efi_mm, paddr, md->virt_addr, size, prot);
+		create_pgd_mapping(&efi_mm, paddr, md->virt_addr, size,
+				   __pgprot(pgprot_val(prot) | PTE_NG));
 	}
 	return true;
 }
@@ -455,14 +458,7 @@ core_initcall(arm64_dmi_init);
 
 static void efi_set_pgd(struct mm_struct *mm)
 {
-	if (mm == &init_mm)
-		cpu_set_reserved_ttbr0();
-	else
-		cpu_switch_mm(mm->pgd, mm);
-
-	local_flush_tlb_all();
-	if (icache_is_aivivt())
-		__local_flush_icache_all();
+	switch_mm(NULL, mm, NULL);
 }
 
 void efi_virtmap_load(void)
