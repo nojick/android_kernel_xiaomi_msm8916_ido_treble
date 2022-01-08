f45a7140f72bc08d5b2130cbfbb6b3c8645ce7c3
nojick <nojick@gmail.com>
2022-01-08 21:45:52 +0300

    Revert "mm: close PageTail race"
    This reverts commit def52acc90faab583b124f3177d55c15d125e2d1.

--- mm/swap.c
+++ mm/swap.c
@@ -83,7 +83,7 @@ static void put_compound_page(struct page *page)
 {
 	if (unlikely(PageTail(page))) {
 		/* __split_huge_page_refcount can run under us */
-		struct page *page_head = compound_head(page);
+		struct page *page_head = compound_trans_head(page);
 
 		/*
 		 * THP can not break up slab pages so avoid taking
@@ -257,7 +257,7 @@ bool __get_page_tail(struct page *page)
 	 */
 	unsigned long flags;
 	bool got;
-	struct page *page_head = compound_head(page);
+	struct page *page_head = compound_trans_head(page);
 
 	/* Ref to put_compound_page() comment. */
 	if (!__compound_tail_refcounted(page_head)) {
