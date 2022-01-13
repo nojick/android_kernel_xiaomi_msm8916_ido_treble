29bc4ae598a1a2c644fb6ee118f6d921540cac3b
nojick <nojick@gmail.com>
2022-01-14 01:45:04 +0300

    no cmd header driver base

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -29,7 +29,6 @@
 #include <linux/dma-contiguous.h>
 #include <linux/vmalloc.h>
 #include <linux/highmem.h>
-#include <linux/cma.h>
 #include <asm/cacheflush.h>
 #include "../ion_priv.h"
 #include "ion_cp_common.h"
