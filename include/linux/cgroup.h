e792126738e71c715ea49bd840f0841be84ea04c
nojick <nojick@gmail.com>
2022-01-19 13:06:46 +0300

    Cgroup.h: revert allow_attach
    allow_attach was removed in the comment (Revert "cgroup: Add generic c group subsystem permission checks"), but a comment was added (cgroup: Fix issues in allow_attach callback) and the build will not be successful because allow_attach is not declared. Fix it and return the function as it was done in 3.18 magically.

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -615,6 +615,8 @@ struct cgroup_subsys {
 	void (*css_free)(struct cgroup_subsys_state *css);
 	void (*css_reset)(struct cgroup_subsys_state *css);
 
+	int (*allow_attach)(struct cgroup_subsys_state *css,
+			    struct cgroup_taskset *tset);
 	int (*can_attach)(struct cgroup_subsys_state *css,
 			  struct cgroup_taskset *tset);
 	void (*cancel_attach)(struct cgroup_subsys_state *css,
@@ -921,7 +923,6 @@ int cgroup_attach_task_to_root(struct task_struct *tsk, int wait);
  */
 int subsys_cgroup_allow_attach(struct cgroup_subsys_state *css,
 			       struct cgroup_taskset *tset);
-
 
 #else /* !CONFIG_CGROUPS */
 
