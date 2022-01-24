40b8334f83892f75a46c7c51e7e1474a605535d6
nojick <nojick@gmail.com>
2022-01-24 14:50:39 +0300

    Revert "sched: Account for cpu's current frequency when calculating its power cost"
    This reverts commit e87706488c63cdcddad9b1e20eb8242e23dc9ef4.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1626,7 +1626,6 @@ unsigned int power_cost_at_freq(int cpu, unsigned int freq)
 static unsigned int power_cost(struct task_struct *p, int cpu)
 {
 	unsigned int demand;
-	unsigned int cur_freq = cpu_rq(cpu)->cur_freq;
 
 	if (!sysctl_sched_enable_power_aware)
 		return cpu_rq(cpu)->capacity;
@@ -1640,8 +1639,6 @@ static unsigned int power_cost(struct task_struct *p, int cpu)
 
 	demand *= cpu_rq(cpu)->max_possible_freq;
 	demand /= 100; /* khz needed */
-
-	demand = max(cur_freq, demand);
 
 	return power_cost_at_freq(cpu, demand);
 }
