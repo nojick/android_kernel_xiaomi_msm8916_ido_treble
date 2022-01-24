c7a70d64f405ec176f29de7cb2139981b18e7140
nojick <nojick@gmail.com>
2022-01-25 00:09:17 +0300

    Proc: Fix the text format. Build error

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -187,8 +187,8 @@ static inline void task_state(struct seq_file *m, struct pid_namespace *ns,
 		"PPid:\t%d\n"
 		"TracerPid:\t%d\n"
 		"Uid:\t%d\t%d\t%d\t%d\n"
-		"Gid:\t%d\t%d\t%d\t%d\n",
-		"Ngid:\t%d\n"
+		"Gid:\t%d\t%d\t%d\t%d\n"
+		"Ngid:\t%d\n",
 		get_task_state(p),
 		leader ? task_pid_nr_ns(leader, ns) : 0,
 		pid_nr_ns(pid, ns),
@@ -200,7 +200,7 @@ static inline void task_state(struct seq_file *m, struct pid_namespace *ns,
 		from_kgid_munged(user_ns, cred->gid),
 		from_kgid_munged(user_ns, cred->egid),
 		from_kgid_munged(user_ns, cred->sgid),
-		from_kgid_munged(user_ns, cred->fsgid)
+		from_kgid_munged(user_ns, cred->fsgid),
 		task_numa_group_id(p));
 
 	task_lock(p);
