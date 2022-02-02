87b103651e18c71f0e4d87e75d3ff9e0f238b9e8
nojick <nojick@gmail.com>
2022-02-02 22:32:34 +0300

    Revert "cpuidle / menu: Return (-1) if there are no suitable states"
    This reverts commit c24d75b7294314d9a07af26a8d062185b9314bea.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -190,8 +190,6 @@ int cpuidle_idle_call(void)
 
 	/* ask the governor for the next state */
 	next_state = cpuidle_select(drv, dev);
-	if (next_state < 0)
-		return -EBUSY;
 
 	if (need_resched()) {
 		dev->last_residency = 0;
