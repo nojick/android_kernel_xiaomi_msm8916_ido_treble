7ba668fa40d5c14a5524e4550274f592cb8ee5ad
nojick <nojick@gmail.com>
2022-02-03 21:44:37 +0300

    Merge branch 'pm-cpuidle' of git://git.kernel.org/pub/scm/linux/kerne…
    Conflicts:
    	kernel/sched/idle.c

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -99,9 +99,10 @@ static void cpuidle_idle_call(void)
 
 	/*
 	 * Ask the cpuidle framework to choose a convenient idle state.
-	 * Fall back to the default arch specific idle method on errors.
+	 * Fall back to the default arch idle method on errors.
 	 */
-	if (cpuidle_enabled(drv, dev)) {
+	next_state = cpuidle_select(drv, dev);
+	if (next_state < 0) {
 use_default:
 		/*
 		 * We can't use the cpuidle framework, let's use the default
@@ -115,12 +116,6 @@ use_default:
 		goto exit_idle;
 	}
 
-	/*
-	 * Ask the governor to choose an idle state it thinks
-	 * it is convenient to go to. There is *always* a
-	 * convenient idle state
-	 */
-	next_state = cpuidle_select(drv, dev);
 
 	/*
 	 * The idle task must be scheduled, it is pointless to
