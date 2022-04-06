81fe6fe26e9b6b985b95356027c62282fb3a2b2e
nojick <nojick@gmail.com>
2022-04-06 10:31:38 +0300

    Revert "ANDROID: fs: FS tracepoints to track IO."
    This reverts commit 00b3da6f1720d10d5a4da4087d0896a28f079151.

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -10,7 +10,6 @@
 
 #include <linux/fs.h>
 #include <linux/f2fs_fs.h>
-#include <trace/events/android_fs.h>
 
 #include "f2fs.h"
 #include "node.h"
@@ -100,8 +99,6 @@ int f2fs_read_inline_data(struct inode *inode, struct page *page)
 
 	ipage = get_node_page(F2FS_I_SB(inode), inode->i_ino);
 	if (IS_ERR(ipage)) {
-		trace_android_fs_dataread_end(inode, page_offset(page),
-					      PAGE_SIZE);
 		unlock_page(page);
 		return PTR_ERR(ipage);
 	}
@@ -119,7 +116,6 @@ int f2fs_read_inline_data(struct inode *inode, struct page *page)
 	if (!PageUptodate(page))
 		SetPageUptodate(page);
 	f2fs_put_page(ipage, 1);
-	trace_android_fs_dataread_end(inode, page_offset(page), PAGE_SIZE);
 	unlock_page(page);
 	return 0;
 }
