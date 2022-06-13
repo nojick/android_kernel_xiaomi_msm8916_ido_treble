ec1990f8e1dd43949082e83c6716295022302ad3
nojick <nojick@gmail.com>
2022-06-14 02:12:51 +0300

    Revert "end"
    This reverts commit 4ee64a52fcc86d896b2bbdac0164a23ca333c111.

--- sound/soc/msm/msm-audio-pinctrl.h
+++ sound/soc/msm/msm-audio-pinctrl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ /* Copyright (c) 2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -42,4 +42,3 @@ int msm_gpioset_activate(enum pinctrl_client client, char *keyword);
 int msm_gpioset_suspend(enum pinctrl_client client, char *keyword);
 
 #endif /* __MSM_AUDIO_PINCTRL_H */
-
