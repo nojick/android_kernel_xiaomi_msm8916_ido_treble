f22528ad1ec87bb7df1d599ae0772c3afdd0d73d
nojick <nojick@gmail.com>
2025-01-13 09:28:08 +0300

    Revert "irqchip: gic: Fix core ID calculation when topology is read from DT"
    This reverts commit 4003b69e90b58f35a60061e6f78a4d98a6ff2727.

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -45,7 +45,6 @@
 #include <linux/syscore_ops.h>
 #include <linux/msm_rtb.h>
 
-#include <asm/cputype.h>
 #include <asm/irq.h>
 #include <asm/exception.h>
 #include <asm/smp_plat.h>
@@ -1009,9 +1008,7 @@ void __init gic_init_bases(unsigned int gic_nr, int irq_start,
 		}
 
 		for_each_possible_cpu(cpu) {
-			u32 mpidr = cpu_logical_map(cpu);
-			u32 core_id = MPIDR_AFFINITY_LEVEL(mpidr, 0);
-			unsigned long offset = percpu_offset * core_id;
+			unsigned long offset = percpu_offset * cpu_logical_map(cpu);
 			*per_cpu_ptr(gic->dist_base.percpu_base, cpu) = dist_base + offset;
 			*per_cpu_ptr(gic->cpu_base.percpu_base, cpu) = cpu_base + offset;
 		}
