734a78fdc859dff8677081b35ad9b7be4869d220
nojick <nojick@gmail.com>
2025-05-01 10:02:02 +0300

    Revert "clk: msm: Add snaphot of msm clock drivers"
    This reverts commit db1e03918c3b825a2218c7ce19172c9c9eb51e44.

--- include/soc/qcom/clock-local2.h
+++ include/soc/qcom/clock-local2.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -54,7 +54,6 @@ struct clk_freq_tbl {
  * @freq_tbl: frequency table for this RCG
  * @current_freq: current RCG frequency
  * @c: generic clock data
- * @non_local_children: set if RCG has at least one branch owned by a diff EE
  * @base: pointer to base address of ioremapped registers.
  */
 struct rcg_clk {
@@ -66,7 +65,6 @@ struct rcg_clk {
 	struct clk_freq_tbl *current_freq;
 	struct clk	c;
 
-	bool non_local_children;
 	void *const __iomem *base;
 };
 
@@ -87,11 +85,6 @@ extern struct clk_freq_tbl rcg_dummy_freq;
  * @cur_div: current branch divider value
  * @max_div: maximum branch divider value (if zero, no divider exists)
  * @halt_check: halt checking type
- * @toggle_memory: toggle memory during enable/disable if true
- * @no_halt_check_on_disable: When set, do not check status bit during
- *			      clk_disable().
- * @check_enable_bit: Check the enable bit to determine clock status
-				during handoff.
  * @base: pointer to base address of ioremapped registers.
  */
 struct branch_clk {
@@ -103,9 +96,6 @@ struct branch_clk {
 	u32 cur_div;
 	u32 max_div;
 	const u32 halt_check;
-	bool toggle_memory;
-	bool no_halt_check_on_disable;
-	bool check_enable_bit;
 	void *const __iomem *base;
 };
 
