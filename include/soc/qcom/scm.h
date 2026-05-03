e1f7a738a4a017d1b7a698493940ea03afc8cdd8
nojick <nojick@gmail.com>
2026-05-03 21:43:13 +0300

    Revert "qcom: scm: Provide an API that restores security configuration"
    This reverts commit 9933a272db9a5612bcc2ee0ef9149f70c8166eb3.

--- include/soc/qcom/scm.h
+++ include/soc/qcom/scm.h
@@ -118,7 +118,6 @@ extern u32 scm_get_version(void);
 extern int scm_is_call_available(u32 svc_id, u32 cmd_id);
 extern int scm_get_feat_version(u32 feat);
 extern bool is_scm_armv8(void);
-extern int scm_restore_sec_cfg(u32 device_id, u32 spare, int *scm_ret);
 extern u32 scm_io_read(phys_addr_t address);
 extern int scm_io_write(phys_addr_t address, u32 val);
 
@@ -205,11 +204,6 @@ static inline int scm_get_feat_version(u32 feat)
 static inline bool is_scm_armv8(void)
 {
 	return true;
-}
-
-static inline int scm_restore_sec_cfg(u32 device_id, u32 spare, int *scm_ret)
-{
-	return 0;
 }
 
 static inline u32 scm_io_read(phys_addr_t address)
