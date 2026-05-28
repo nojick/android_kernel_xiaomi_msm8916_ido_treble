fb7a71f52b0601ad259b05f2530a1b47efa38e06
nojick <nojick@gmail.com>
2026-05-28 21:20:14 +0300

    fix build uostream pstore

--- drivers/lastkmsg/last_kmsg.c
+++ drivers/lastkmsg/last_kmsg.c
@@ -13,7 +13,6 @@
  */
 
 #include <linux/slab.h>
-
 #include <linux/atomic.h>
 #include <linux/console.h>
 #include <linux/delay.h>
@@ -34,10 +33,13 @@
 #include <linux/of_reserved_mem.h>
 #include <linux/pstore.h>
 #include <linux/io.h>
+
 extern struct pstore_info *psinfo;
 
 #ifdef CONFIG_PSTORE
-void pstore_console_show(enum pstore_type_id type_id, struct seq_file *m, void *v)
+void pstore_console_show(enum pstore_type_id type_id,
+			struct seq_file *m,
+			void *v)
 {
 	struct pstore_info *psi = psinfo;
 	char *buf = NULL;
@@ -46,16 +48,17 @@ void pstore_console_show(enum pstore_type_id type_id, struct seq_file *m, void *
 	int count;
 	enum pstore_type_id type;
 	struct timespec time;
+	bool compressed;
 
 	if (!psi)
 		return;
+
 	mutex_lock(&psi->read_mutex);
 	if (psi->open && psi->open(psi))
 		goto out;
 
-	while ((size = psi->read(&id, &type, &count, &time, &buf, psi)) > 0) {
-		/*pr_err("ram_console: id %lld, type %d, count %d, size %zx\n", id, type, count,
-		       size);*/
+	while ((size = psi->read(&id, &type, &count, &time, &buf,
+				&compressed, psi)) > 0) {
 		if (type == type_id)
 			seq_write(m, buf, size);
 		kfree(buf);
@@ -64,29 +67,26 @@ void pstore_console_show(enum pstore_type_id type_id, struct seq_file *m, void *
 
 	if (psi->close)
 		psi->close(psi);
+
 out:
 	mutex_unlock(&psi->read_mutex);
 }
 #endif
 
-static int ram_console_lastk_show( struct seq_file *m, void *v)
+static int ram_console_lastk_show(struct seq_file *m, void *v)
 {
-
 	pr_err("ram_console_lastk_show\n");
 
 #ifdef CONFIG_PSTORE_CONSOLE
-	/*pr_err("ram_console: pstore show start\n");*/
 	pstore_console_show(PSTORE_TYPE_CONSOLE, m, v);
-	/*pr_err("ram_console: pstore show end\n");*/
 #endif
 	return 0;
 }
 
-
-
 static int ram_console_show(struct seq_file *m, void *v)
 {
-	ram_console_lastk_show( m, v);
+	ram_console_lastk_show(m, v);
+
 	return 0;
 }
 
@@ -110,12 +110,10 @@ static int __init ram_console_late_init(void)
 	entry = proc_create("last_kmsg", 0444, NULL, &ram_console_file_ops);
 	if (!entry) {
 		pr_err("ram_console: failed to create proc entry\n");
-
 		return 0;
 	}
+
 	return 0;
 }
 
-
 late_initcall(ram_console_late_init);
-
