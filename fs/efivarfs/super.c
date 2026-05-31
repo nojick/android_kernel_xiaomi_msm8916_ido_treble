9d6c0f4494a50d50a65a4a5311135ef5bff75077
nojick <nojick@gmail.com>
2026-05-31 19:21:56 +0300

    Revert "efi: Make efivarfs entries immutable by default"
    This reverts commit 7b35014c77e1ba732617c83ab6705298302bd09f.

--- fs/efivarfs/super.c
+++ fs/efivarfs/super.c
@@ -120,7 +120,6 @@ static int efivarfs_callback(efi_char16_t *name16, efi_guid_t vendor,
 	char *name;
 	int len;
 	int err = -ENOMEM;
-	bool is_removable = false;
 
 	entry = kmalloc(sizeof(*entry), GFP_KERNEL);
 	if (!entry)
@@ -138,17 +137,13 @@ static int efivarfs_callback(efi_char16_t *name16, efi_guid_t vendor,
 
 	ucs2_as_utf8(name, entry->var.VariableName, len);
 
-	if (efivar_variable_is_removable(entry->var.VendorGuid, name, len))
-		is_removable = true;
-
 	name[len] = '-';
 
 	efi_guid_unparse(&entry->var.VendorGuid, name + len + 1);
 
 	name[len + EFI_VARIABLE_GUID_LEN+1] = '\0';
 
-	inode = efivarfs_get_inode(sb, root->d_inode, S_IFREG | 0644, 0,
-				   is_removable);
+	inode = efivarfs_get_inode(sb, root->d_inode, S_IFREG | 0644, 0);
 	if (!inode)
 		goto fail_name;
 
@@ -204,7 +199,7 @@ static int efivarfs_fill_super(struct super_block *sb, void *data, int silent)
 	sb->s_d_op		= &efivarfs_d_ops;
 	sb->s_time_gran         = 1;
 
-	inode = efivarfs_get_inode(sb, NULL, S_IFDIR | 0755, 0, true);
+	inode = efivarfs_get_inode(sb, NULL, S_IFDIR | 0755, 0);
 	if (!inode)
 		return -ENOMEM;
 	inode->i_op = &efivarfs_dir_inode_operations;
