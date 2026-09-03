f980dea10db37d0f93bec83a3e9e2c495ba4cc01
nojick <nojick@gmail.com>
2026-09-03 19:42:04 +0300

    Revert "msm: vidc: program QDSS mapping addresses based on target"
    This reverts commit 17df736110995ea10919f345f7bc4039e2d8a1c1.

--- drivers/media/platform/msm/vidc/msm_vidc_resources.h
+++ drivers/media/platform/msm/vidc/msm_vidc_resources.h
@@ -41,11 +41,6 @@ struct addr_range {
 	u32 size;
 };
 
-struct addr_set {
-	struct addr_range *addr_tbl;
-	int count;
-};
-
 struct iommu_info {
 	const char *name;
 	u32 buffer_type[MAX_BUFFER_TYPES];
@@ -122,7 +117,6 @@ struct msm_vidc_platform_resources {
 	struct load_freq_table *load_freq_tbl;
 	uint32_t load_freq_tbl_size;
 	struct reg_set reg_set;
-	struct addr_set qdss_addr_set;
 	struct iommu_set iommu_group_set;
 	struct buffer_usage_set buffer_usage_set;
 	uint32_t imem_size;
