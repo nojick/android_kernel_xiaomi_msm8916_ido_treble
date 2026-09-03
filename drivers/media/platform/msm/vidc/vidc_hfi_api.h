40192ad4175d08031f9a8ad97981c328600d58f4
nojick <nojick@gmail.com>
2026-09-03 19:55:15 +0300

    Revert "msm: vidc: Support upstream SMMU APIs"
    This reverts commit 45fa2844b2eab807fe6439dacd5f58fa3c9b244d.

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -1357,6 +1357,8 @@ struct hfi_device {
 	int (*vote_bus)(void *dev, struct vidc_bus_vote_data *data,
 			int num_data);
 	int (*unvote_bus)(void *dev);
+	int (*iommu_get_domain_partition)(void *dev, u32 flags, u32 buffer_type,
+			int *domain_num, int *partition_num);
 	int (*load_fw)(void *dev);
 	void (*unload_fw)(void *dev);
 	int (*resurrect_fw)(void *dev);
