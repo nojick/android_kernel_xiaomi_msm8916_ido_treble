0f001fa858820469246ab2efa1c13f17dc9b81ec
nojick <nojick@gmail.com>
2026-09-08 16:58:33 +0300

    Revert "msm: vidc: Check for active buffers only in registered list"
    This reverts commit a88d2ef2d83c8654546495a1002f80f0061cf5aa.

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -268,8 +268,7 @@ struct buffer_info *get_registered_buf(struct msm_vidc_inst *inst,
 			bool overlaps = OVERLAPS(buff_off, size,
 					temp->buff_off[i], temp->size[i]);
 
-			if (!temp->inactive &&
-				(fd_matches || device_addr_matches) &&
+			if ((fd_matches || device_addr_matches) &&
 				(contains_within || overlaps)) {
 				dprintk(VIDC_DBG,
 						"This memory region is already mapped\n");
