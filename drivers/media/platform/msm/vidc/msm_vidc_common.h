7412afa9ea80e758e76ed96b33d396583fa44cd8
nojick <nojick@gmail.com>
2026-09-17 23:04:52 +0300

    Revert "Revert "msm: vidc: Add Encoder DCVS support""
    This reverts commit 36b28cc2d586c5095a67882c41e0b8193275a3c1.

--- drivers/media/platform/msm/vidc/msm_vidc_common.h
+++ drivers/media/platform/msm/vidc/msm_vidc_common.h
@@ -19,7 +19,15 @@ struct vb2_buf_entry {
 	struct vb2_buffer *vb;
 };
 
+
 extern const char *const mpeg_video_vidc_extradata[];
+
+enum load_calc_quirks {
+	LOAD_CALC_NO_QUIRKS = 0,
+	LOAD_CALC_IGNORE_TURBO_LOAD = 1 << 0,
+	LOAD_CALC_IGNORE_THUMBNAIL_LOAD = 1 << 1,
+	LOAD_CALC_IGNORE_NON_REALTIME_LOAD = 1 << 2,
+};
 
 struct msm_vidc_core *get_vidc_core(int core_id);
 const struct msm_vidc_format *msm_comm_get_pixel_fmt_index(
@@ -40,8 +48,8 @@ int msm_comm_set_output_buffers(struct msm_vidc_inst *inst);
 int msm_comm_queue_output_buffers(struct msm_vidc_inst *inst);
 int msm_comm_qbuf(struct vb2_buffer *vb);
 void msm_comm_scale_clocks_and_bus(struct msm_vidc_inst *inst);
-void msm_comm_init_dcvs(struct msm_vidc_inst *inst);
-void msm_comm_init_dcvs_load(struct msm_vidc_inst *inst);
+int msm_comm_scale_clocks(struct msm_vidc_core *core);
+int msm_comm_scale_clocks_load(struct msm_vidc_core *core, int num_mbs_per_sec);
 int msm_comm_flush(struct msm_vidc_inst *inst, u32 flags);
 int msm_comm_release_scratch_buffers(struct msm_vidc_inst *inst,
 					bool check_for_reuse);
@@ -76,4 +84,8 @@ int msm_comm_smem_get_domain_partition(struct msm_vidc_inst *inst,
 			u32 flags, enum hal_buffer buffer_type,
 			int *domain_num, int *partition_num);
 enum hal_video_codec get_hal_codec_type(int fourcc);
+int msm_comm_get_inst_load(struct msm_vidc_inst *inst,
+			enum load_calc_quirks quirks);
+int msm_comm_get_load(struct msm_vidc_core *core,
+			enum session_type type, enum load_calc_quirks quirks);
 #endif
